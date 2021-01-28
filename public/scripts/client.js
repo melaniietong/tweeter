d6b070f6c88f29f87d3136a2a1abf30ac6888978
Melanie Tong <melaniietong@gmail.com>
2021-01-28 23:14:24 +0000

    Remove temp tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1611612975892
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1611699375892
-  },
-];
-
 const createTweetElement = function(data) {
   let tweets = [];
   for (let post of data) {
